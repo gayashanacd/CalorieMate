76916c30e0df441248b4038330f0865a25e580f5
Gayashan <gayazhanmit@yahoo.com>
2024-07-04 12:24:50 -0700

    added create program screens

---
+++ app/src/main/java/com/example/caloriemate/ui/program/ProgramActivityStep1.java
@@ -0,0 +1,16 @@
+package com.example.caloriemate.ui.program;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.caloriemate.R;
+
+public class ProgramActivityStep1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_program_step1);
+    }
+}
